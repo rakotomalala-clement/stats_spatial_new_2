141cd2e362a947aa47ce081588b2d10ed49ea02d
rakotomalala-clement <clement.rakotomalala@eleve.ensai.fr>
2025-02-27 11:48:26 +0000

    hfybfybdyubziobuiouzeb

--- exercice1.R
+++ exercice1.R
@@ -31,4 +31,39 @@ dept_aura <- communes_AURA %>%
 
 plot(dept_aura)
 
-dept_aura2 <- communes_AURA %>% ddd
+dept_aura2 <- communes_AURA %>% 
+  group_by(dep) %>% 
+  summarise(geom = st_union(geom))
+
+plot(dept_aura2)
+
+
+#14
+
+centroid_dept_aura <- st_centroid(dept_aura2)
+plot(st_geometry(dept_aura2))
+plot(st_geometry(centroid_dept_aura), add=TRUE)
+
+departements_AURA <- c("Ain", "Allier", "Ardèche", "Cantal", "Drôme", 
+                       "Isère", "Loire", "Haute-Loire", "Puy-de-Dôme", 
+                       "Rhône", "Savoie", "Haute-Savoie")
+
+data_departements_AURA <- data.frame(dept_lib = departements_AURA)
+
+centroid_dept_aura <- cbind(centroid_dept_aura, data_departements_AURA)
+
+centroid_cords <- st_coordinates(centroid_dept_aura)
+
+centroid_cords <- centroid_cords %>% 
+  bind_cols(
+    centroid_dept_aura %>% 
+      select(dep, dept_lib) %>% 
+      st_drop_geometry()
+  )
+
+plot(st_geometry(dept_aura2))
+plot(st_geometry(centroid_dept_aura), add=TRUE)
+text(x=centroid_cords$X, y=centroid_cords$Y, labels=centroid_cords$dept_lib, pos = 3, cex = 0.8,
+     col = "orange")
+
+st_intersection(centroid_dept_aura, communes_AURA)
